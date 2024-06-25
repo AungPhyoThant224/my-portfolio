afb88e0351123e2a6d10973a9734c137be9b8cb8
Aung Phyo Thant <aungphyothant224@gmail.com>
2024-06-25 21:39:40 +0630

    - Error Page.

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -1,48 +1,79 @@
-import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Text } from "@chakra-ui/react";
+import parse from "html-react-parser";
 import { Link } from "react-router-dom";
 import Section from "../../components/Section";
-import { primaryBackground } from "../../theme/colors";
-import { AungPhyoThant } from "../../assets/images";
+import Terminal from "../../components/Terminal";
+import navBar from "../../data/navBar";
+import { primaryBackground, primaryGradientBorder } from "../../theme/colors";
 
 const ErrorPage = () => {
   return (
     <Box background={primaryBackground()} height={"100vh"}>
       <Section
         id="nav-bar"
-        maxWidth="1200px"
+        maxWidth="1140px"
         background={primaryBackground()}
         as={"nav"}
       >
-        <Grid templateColumns="repeat(2, 1fr)">
+        <Grid templateColumns="230px auto" alignItems={"center"}>
           <GridItem>
             <Link to="/" style={{ textDecoration: "none" }}>
-              <Box
-                textAlign={"center"}
-                width={"fit-content"}
-                paddingTop={5}
-                paddingBottom={6}
-              >
-                Home
+              <Box textAlign={"center"} width={"fit-content"} paddingY={5}>
+                <Text fontFamily={"Tiny5"} fontWeight={"bold"} fontSize={"3xl"}>
+                  {parse(navBar.title)}
+                </Text>
               </Box>
             </Link>
           </GridItem>
         </Grid>
+        <Box
+          bgGradient={primaryGradientBorder()}
+          width={"100%"}
+          height={"1px"}
+        ></Box>
       </Section>
       <Section
         id="Error"
-        maxWidth="1200px"
+        maxWidth="1140px"
         background={primaryBackground()}
         as={"section"}
       >
-        <Box>
-          <Image
-            data-aos="zoom-in"
-            src={AungPhyoThant}
-            alt="404"
-            width={620}
-            height={"100%"}
-            margin={"auto"}
-          />
+        <Box
+          data-aos="zoom-in"
+          data-aos-duration="300"
+          data-aos-easing="linear"
+          height={"100vh"}
+          width={"fit-content"}
+          margin={"auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Terminal title="Error.js">
+            <Box textAlign={"center"} padding={8}>
+              <Text
+                fontSize={{ base: "7xl", sm: "9xl" }}
+                fontFamily={"monospace"}
+                lineHeight={1}
+                color={"whiteAlpha.900"}
+              >
+                OOPS!
+              </Text>
+              <Text
+                fontFamily={"monospace"}
+                fontSize={{ base: "xl", sm: "3xl" }}
+                color={"whiteAlpha.900"}
+              >
+                Something went wrong.
+              </Text>
+            </Box>
+          </Terminal>
+          <Box marginY={4}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              {"<- Go Back"}
+            </Link>
+          </Box>
         </Box>
       </Section>
     </Box>
